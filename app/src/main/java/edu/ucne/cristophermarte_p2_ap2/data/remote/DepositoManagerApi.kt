329d445941b82693e84fd9d01e18238631b2491b
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-26 20:35:40 -0400

    Se agregaron funciones faltantes.

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/data/remote/DepositoManagerApi.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/data/remote/DepositoManagerApi.kt
@@ -1,9 +1,13 @@
 package edu.ucne.cristophermarte_p2_ap2.data.remote
 
 import edu.ucne.cristophermarte_p2_ap2.data.remote.dto.DepositoDto
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface DepositoManagerApi {
     @GET("/api/Depositos")
@@ -11,4 +15,16 @@ interface DepositoManagerApi {
 
     @POST("/api/Depositos")
     suspend fun saveDeposito(@Body deposito: DepositoDto?): DepositoDto
+
+    @GET("/api/Depositos/{depositoId}")
+    suspend fun getDepositoById(@Path("depositoId") depositoId: Int): DepositoDto
+
+    @PUT("/api/Depositos/{depositoId}")
+    suspend fun updateDeposito(
+        @Path("depositoId") depositoId: Int,
+        @Body deposito: DepositoDto
+    ): Response<DepositoDto>
+
+    @DELETE("/api/Depositos/{depositoId}")
+    suspend fun deleteDeposito(@Path("depositoId") depositoId: Int): Response<Unit>
 }
