fbd137432d12090d986e09405619d35dbfce76d1
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-27 00:27:21 -0400

    Agregando "@AndroidEntryPoint" al MainActivity.

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/MainActivity.kt
@@ -5,17 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.cristophermarte_p2_ap2.presentation.navigation.RegistroNavHost
 import edu.ucne.cristophermarte_p2_ap2.ui.theme.CristopherMarte_P2_AP2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CristopherMarte_P2_AP2Theme {
-                val navHost = rememberNavController()
-                RegistroNavHost(navHost)
+                RegistroNavHost(rememberNavController())
             }
         }
     }
