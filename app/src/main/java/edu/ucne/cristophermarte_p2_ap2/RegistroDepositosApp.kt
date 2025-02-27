2fbc29fbc94f68c8c93626624001f327a9aa042a
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-27 00:27:37 -0400

    Se creó la clase `RegistroDepositosApp` para la configuración de Hilt.

---
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/RegistroDepositosApp.kt
@@ -0,0 +1,8 @@
+package edu.ucne.cristophermarte_p2_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroDepositosApp: Application(){
+}
