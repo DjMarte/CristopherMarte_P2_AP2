109b0a061b76e2d942ec7556ded912436a66476d
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-26 20:34:58 -0400

    Se creó la pantalla de la consula del Deposito.

---
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/presentation/depositos/DepositoListScreen.kt
@@ -0,0 +1,215 @@
+package edu.ucne.cristophermarte_p2_ap2.presentation.depositos
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.cristophermarte_p2_ap2.data.remote.dto.DepositoDto
+import edu.ucne.cristophermarte_p2_ap2.presentation.navigation.Screen
+
+@Composable
+fun DepositoListScreen(
+    viewModel: DepositoViewModel = hiltViewModel(),
+    onAddDeposito: () -> Unit,
+    goToDepositoScreen: (Int) -> Unit
+){
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    DepositoBodyListScreen(
+        uiState = uiState,
+        onAddDeposito = onAddDeposito,
+        goToDepositoScreen = goToDepositoScreen
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DepositoBodyListScreen(
+    uiState: DepositoUiState,
+    onAddDeposito: () -> Unit,
+    goToDepositoScreen: (Int) -> Unit
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Listado de Depositos") }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddDeposito) {
+                Icon(Icons.Default.Add, contentDescription = "Agregar deposito")
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            if (uiState.isLoading) {
+                CircularProgressIndicator()
+            } else {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    TableHeader()
+                    uiState.errorMessage?.let { error ->
+                        Text(
+                            text = error,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        items(uiState.listadeposito) { deposito ->
+                            DepositoRow(deposito, goToDepositoScreen)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TableHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "IdDeposito",
+            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "Fecha",
+            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "idCuenta",
+            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "Concepto",
+            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = "Monto",
+            style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+    HorizontalDivider(modifier = Modifier.padding(vertical = 5.dp))
+}
+
+@Composable
+private fun DepositoRow(
+    it: DepositoDto,
+    goToDepositoScreen: (Int) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(15.dp)
+            .clickable { goToDepositoScreen(it.depositoId) },
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.depositoId.toString(),
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.fecha.toString(),
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.cuentaId.toString(),
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.concepto,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.monto.toString(),
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+    HorizontalDivider(modifier = Modifier.padding(vertical = 5.dp))
+}
