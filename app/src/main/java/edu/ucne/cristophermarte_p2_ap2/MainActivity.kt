f1a5972b057557e075eab95e8770e391af156e99
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-26 20:37:35 -0400

    Agregué la navegación usando `RegistroNavHost` y `rememberNavController` en `MainActivity`.

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import edu.ucne.cristophermarte_p2_ap2.presentation.navigation.RegistroNavHost
 import edu.ucne.cristophermarte_p2_ap2.ui.theme.CristopherMarte_P2_AP2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CristopherMarte_P2_AP2Theme {
-
+                val navHost = rememberNavController()
+                RegistroNavHost(navHost)
             }
         }
     }
