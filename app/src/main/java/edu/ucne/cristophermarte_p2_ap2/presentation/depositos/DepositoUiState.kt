8787d5c8f661e324933a1c670f457bec313cadad
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-27 00:26:35 -0400

    Cambiando el tipo de dato de la fecha, de "Date" a "String"

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/presentation/depositos/DepositoUiState.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/presentation/depositos/DepositoUiState.kt
@@ -1,11 +1,10 @@
 package edu.ucne.cristophermarte_p2_ap2.presentation.depositos
 
 import edu.ucne.cristophermarte_p2_ap2.data.remote.dto.DepositoDto
-import java.util.Date
 
 data class DepositoUiState(
     val depositoId: Int = 0,
-    val fecha: Date = Date(),
+    val fecha: String = "",
     val fechaSeleccionada: Boolean = false,
     val cuentaId: Int = 0,
     val concepto: String = "",
