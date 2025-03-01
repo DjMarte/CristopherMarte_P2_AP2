026764657640fffbf4d7304e45aab8c7639aa354
DjMarte <cristopher_marte@ucne.edu.do>
2025-03-01 15:56:24 -0400

    Renombrando correctamente un método y la entidad pasada en la base de datos.

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/data/local/database/CristopherMarteP2Db.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/data/local/database/CristopherMarteP2Db.kt
@@ -2,16 +2,16 @@ package edu.ucne.cristophermarte_p2_ap2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.cristophermarte_p2_ap2.data.local.dao.EntidadDao
-import edu.ucne.cristophermarte_p2_ap2.data.local.entity.EntidadEntity
+import edu.ucne.cristophermarte_p2_ap2.data.local.dao.DepositoDao
+import edu.ucne.cristophermarte_p2_ap2.data.local.entity.DepositoEntity
 
 @Database(
     entities = [
-        EntidadEntity::class
+        DepositoEntity::class
     ],
     version = 1,
     exportSchema = false
 )
 abstract class AdministracionDb : RoomDatabase() {
-    abstract fun entidadDao(): EntidadDao
+    abstract fun depositoDao(): DepositoDao
 }
