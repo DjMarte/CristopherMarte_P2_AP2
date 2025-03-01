f493622fa241f7b7ec0124a0a63ca406cc82d5b1
DjMarte <cristopher_marte@ucne.edu.do>
2025-03-01 15:00:55 -0400

    Se modificó el tipo de dato de la lista `listadeposito` a `DepositoEntity`.

--- app/src/main/java/edu/ucne/cristophermarte_p2_ap2/presentation/depositos/DepositoUiState.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p2_ap2/presentation/depositos/DepositoUiState.kt
@@ -1,6 +1,6 @@
 package edu.ucne.cristophermarte_p2_ap2.presentation.depositos
 
-import edu.ucne.cristophermarte_p2_ap2.data.remote.dto.DepositoDto
+import edu.ucne.cristophermarte_p2_ap2.data.local.entity.DepositoEntity
 
 data class DepositoUiState(
     val depositoId: Int = 0,
@@ -10,6 +10,6 @@ data class DepositoUiState(
     val concepto: String = "",
     val monto: Double = 0.0,
     val errorMessage: String? = null,
-    val listadeposito: List<DepositoDto> = emptyList(),
+    val listadeposito: List<DepositoEntity> = emptyList(),
     val isLoading: Boolean = false
 )
